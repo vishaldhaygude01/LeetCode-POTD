0f2bee325e22259b8f631d5cf9093e483c24d6c1
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2023-01-19 21:11:23 +0530

    commit

--- subarray_sum_divisible_by_k.cpp
+++ subarray_sum_divisible_by_k.cpp
@@ -5,16 +5,16 @@ public:
     {
         int sum = 0;
         int ans = 0;
-        vector<int> ans(k);
-        ans[0] = 1;
+        vector<int> a(k);
+        a[0] = 1;
 
         int rem;
         for (int num : nums)
         {
             sum += num;
             rem = (sum % k + k) % k;
-            ans += ans[rem];
-            ans[rem]++;
+            ans += a[rem];
+            a[rem]++;
         }
         return ans;
     }
