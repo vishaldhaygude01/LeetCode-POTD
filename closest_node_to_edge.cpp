67309fabcba26ee88e2ca0607dde11503a5f9f84
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2023-01-25 19:47:24 +0530

    commit

--- closest_node_to_edge.cpp
+++ closest_node_to_edge.cpp
@@ -1,35 +1,38 @@
-class Solution
-{
+class Solution {
 public:
-    int closestMeetingNode(vector<int> &edges, int n1, int n2)
-    {
-        if (n1 == n2)
-            return n1;
-        int n = size(edges);
-        vector<int> vis(n);
-        while (n-- && (n1 != -1 || n2 != -1))
-        {
-            int ans = -1;
-            if (n1 != -1)
-            {
-                if (vis[n1] == 2)
-                    ans = n1;
-                vis[n1] = 1;
-                n1 = edges[n1];
-            }
-            if (n2 != -1)
-            {
-                if (vis[n2] == 1)
-                {
-                    if (ans == -1 or n2 < ans)
-                        ans = n2;
-                };
-                vis[n2] = 2;
-                n2 = edges[n2];
-            }
-            if (ans != -1)
-                return ans;
+    int n;  
+    void dfs(vector<int>& edges, int startNode, vector<int>& dist_node, vector<bool>& visited) {
+        visited[startNode] = true;
+        int v = edges[startNode];
+        if(v != -1 && !visited[v]) {
+            visited[v] = true;
+            dist_node[v] = 1 + dist_node[startNode];
+            dfs(edges, v, dist_node, visited);
         }
-        return -1;
+    }
+    
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        n = edges.size(); 
+        vector<int> dist1(n, INT_MAX);
+        vector<int> dist2(n, INT_MAX);
+        vector<bool> visited1(n, false);
+        vector<bool> visited2(n, false);
+        dist1[node1] = 0;
+        dist2[node2] = 0;
+        
+        dfs(edges, node1, dist1, visited1);
+        dfs(edges, node2, dist2, visited2);
+        
+        int minDistNode    = -1;
+        int minDistTillNow = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            int maxD = max(dist1[i], dist2[i]);
+            
+            if (minDistTillNow > maxD) {
+                minDistNode = i;
+                minDistTillNow = maxD;
+            }
+        }
+        return minDistNode;
     }
 };
