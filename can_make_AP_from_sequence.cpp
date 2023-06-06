741f0f84bc0c1adf3489f0abbf38f206adb04805
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2023-06-06 18:44:46 +0530

    commit

---
+++ can_make_AP_from_sequence.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    bool canMakeArithmeticProgression(vector<int> &arr)
+    {
+        int n = arr.size();
+        sort(begin(arr), end(arr));
+
+        int d = arr[1] - arr[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] - arr[i - 1] != d)
+                return false;
+        }
+
+        return true;
+    }
+};
