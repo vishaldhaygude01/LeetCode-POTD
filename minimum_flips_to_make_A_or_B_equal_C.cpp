7bfacb38eea90c67a630d39168d27a97caf2a911
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2023-06-07 23:09:04 +0530

    commit

--- minimum_flips_to_make_A_or_B_equal_C.cpp
+++ minimum_flips_to_make_A_or_B_equal_C.cpp
@@ -7,3 +7,31 @@ public:
         return __builtin_popcount(result) + __builtin_popcount((a & b) & (result));
     }
 };
+
+/*
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int result = 0;
+
+        while( a != 0 || b != 0 || c != 0) {
+
+            if( (c & 1) == 1) {
+                if((a & 1) == 0 && (b & 1) == 0) {
+                    result++;
+                }
+            } else {
+                result += (a & 1) + (b & 1);
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+
+        }
+
+        return result;
+    }
+};
+
+*/
