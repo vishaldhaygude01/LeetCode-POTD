b32c650f9342c86a5b45c1484d2db61cf1e80073
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2023-06-09 20:09:17 +0530

    commit

---
+++ smallest_letter_greater_than_target.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+public:
+    char nextGreatestLetter(vector<char> &letters, char target)
+    {
+        int start = 0;
+        int end = letters.size() - 1;
+
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+            if (target < letters[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.size()];
+    }
+};
