711f08636658f4dc10df830067b0c934a2c7af96
Vishal Dhaygude <vishaldhaigude0102@gmail.com>
2023-07-08 10:31:35 +0530

    commit

--- put_marble_in_bag.cpp
+++ put_marble_in_bag.cpp
@@ -24,3 +24,27 @@ public:
         return (right - left);
     }
 };
+
+/*
+  Priority_Queue
+
+  class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        priority_queue<int> pqMin;
+        priority_queue<int, vector<int>, greater<int>> pqMax;
+        for(int i = 1; i < weights.size(); ++i){
+            pqMax.push(weights[i] + weights[i-1]);
+            pqMin.push(weights[i] + weights[i-1]);
+            if(pqMax.size() >= k) { pqMax.pop(); pqMin.pop(); }
+        }
+        long long ans = 0;
+        while(pqMax.size() > 0){
+            ans += pqMax.top() - pqMin.top();
+            pqMax.pop(); pqMin.pop();
+        }
+        return ans;
+    }
+};
+
+*/
